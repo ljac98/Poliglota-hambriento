59b087340f26e1e0ba254ed1ae6cda571876f5c4
ljac98 <luisjac98@outlook.com>
2026-03-20 00:39:03 -0400

    fix(online): importar PLAYER_COLORS en lobby

--- app/screens/OnlineLobby.jsx
+++ app/screens/OnlineLobby.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import socket from '../../src/socket.js';
-import { LANGUAGES, LANG_BORDER, LANG_BG, LANG_TEXT } from '../../constants/index.js';
+import { LANGUAGES, LANG_BORDER, LANG_BG, LANG_TEXT, PLAYER_COLORS } from '../../constants/index.js';
 import { getFriends, sendFriendRequest } from '../../src/api.js';
 import { HatBadge, PercheroSVG } from '../../components/HatComponents.jsx';
 import HatSVG from '../../components/HatSVG.jsx';
